76e8f4c89ab601ea704ff7bf37053e201d501375
mjkim001 <dev.kimminji@gmail.com>
2023-03-08 22:13:54 +0900

    [BOJ] 백준 문제 풀이

---
+++ src/BOJ_2164.java
@@ -0,0 +1,22 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class BOJ_2164 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Queue<Integer> q = new LinkedList<>();
+
+        int N = sc.nextInt();
+
+        for(int i = 1; i <= N; i++) {
+            q.offer(i);
+        }
+
+        while(q.size() > 1) {
+            q.poll();
+            q.offer(q.poll());	// 맨 앞의 원소를 버림과 동시에 버려진 원소를 맨 뒤에 삽입
+        }
+        System.out.println(q.poll());	// 마지막으로 남은 원소 출력
+    }
+}
