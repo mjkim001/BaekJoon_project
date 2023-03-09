8d0824727c40b0ba198022902fde9d2cf4935fa6
mjkim001 <dev.kimminji@gmail.com>
2023-03-09 22:31:26 +0900

    [BOJ] 백준 문제 풀이

---
+++ src/BOJ_2231.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class BOJ_2231 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int res = 0;
+
+        for(int i = 0; i < N; i++) {
+            int num = i;
+            int sum = 0;
+
+            while(num != 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+
+            if(sum + i == N) {
+                res = i;
+                break;
+            }
+        }
+        System.out.println(res);
+    }
+}
